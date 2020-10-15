9aeb4d97f074b23153d63ec7794ff18c1efa8b37
tamakiii <tamakiii@users.noreply.github.com>
2020-10-16 02:27:14 +0900

    .

--- docker.mk
+++ docker.mk
@@ -1,6 +1,6 @@
 .PHONY: setup dependencies list clean
 
-NAME := tamakiii-sandbox/docker-sandbox
+PREFIX := tamakiii-sandbox/docker-sandbox
 DIR := $(realpath $(dir $(firstword $(MAKEFILE_LIST))))
 TYPES := $(patsubst $(basename $(DIR))/%.dockerfile,%,$(wildcard $(DIR)/*.dockerfile))
 TARGETS := $(shell echo $(wildcard $(DIR)/*.dockerfile) | xargs grep -e 'FROM' -e 'AS' | awk '{ print $$4 }')
@@ -14,23 +14,23 @@ dependencies:
 list:
 	@echo $(foreach y,$(TYPES),$(foreach t,$(TARGETS),$y/$t))
 
-# build/$(TYPE)/$(TARGET)
+# build/$(TYPES)/$(TARGETS)
 $(addprefix build/,$(foreach y,$(TYPES),$(foreach t,$(TARGETS),$y/$t))):
 	docker build \
-		-t $(NAME)/$(word 2,$(subst /, ,$@))/$(word 3,$(subst /, ,$@)) \
+		-t $(PREFIX)/$(word 2,$(subst /, ,$@))/$(word 3,$(subst /, ,$@)) \
 		-f $(DIR)/$(word 2,$(subst /, ,$@)).dockerfile \
 		--target $(word 3,$(subst /, ,$@)) \
 		.
 
-# bash/$(TYPE)/$(TARGET)
+# bash/$(TYPES)/$(TARGETS)
 $(addprefix bash/,$(foreach y,$(TYPES),$(foreach t,$(TARGETS),$y/$t))):
 	docker run \
 		-it \
 		--rm \
 		--volume $(shell pwd):/work \
 		--workdir /work \
-		$(NAME)/$(word 2,$(subst /, ,$@))/$(word 3,$(subst /, ,$@)) \
+		$(PREFIX)/$(word 2,$(subst /, ,$@))/$(word 3,$(subst /, ,$@)) \
 		$(word 1,$(subst /, ,$@))
 
 clean:
-	$(foreach y,$(TYPES),docker image rm $(NAME)/$(TYPES))
+	docker image rm $(foreach y,$(TYPES),$(foreach t,$(TARGETS),$(PREFIX)/$y/$t))
